81eb9e25181e055ce753df209971cff88402f346
Trisha-shii <7trisha77@gmail.com>
2025-11-03 08:45:44 +0530

    Added

---
+++ Assets/movement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator animator;
+    public float InputX;
+    public float InputY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputY = Input.GetAxis("Vertical");
+        animator.SetFloat("InputX", InputX);
+        animator.SetFloat("InputY", InputY);
+    }
+}
